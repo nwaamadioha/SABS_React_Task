d249ecc520ac3d45539fbb4f6a549e8429afc6ce
nwaamadioha <udokwu.temple960@gmail.com>
2022-10-04 14:04:19 +0100

    changed CSS properties for the button

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -6,12 +6,35 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   margin-top: 20px;
+
 `
 
 const Wrapper = styled.div`
   justify-content: space-between;
 
 `
+
+const Input = styled.input`
+  width: 50%;
+  border: solid 0.5px;
+  padding: 1px;
+  height: 36px;
+
+`
+const Button = styled.button`
+  position: relative;
+  float: right;
+  margin-right: 5px;
+  color: black;
+  border: none;
+  width: 36px;
+  height: 36px;
+  cursor: pointer;
+  
+`
+
+
+
 const CreateArea = (props) => {
     const [todoItem, setTodoItem] = useState("")
     const [active, setActive] = useState(true)
@@ -26,13 +49,13 @@ const CreateArea = (props) => {
   return (
     <Container>
       <Wrapper>
-        <input
+        <Input
           name = "listItem"
           onChange={(e) => setTodoItem(e.target.value)}
           onClick={() => setActive(false)}
           value={todoItem}
         />
-        <button disabled={active} onClick={handleClick}>Add</button>
+        <Button disabled={active} onClick={handleClick}>Add</Button>
       </Wrapper>
 
     </Container>
